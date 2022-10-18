ee8b74ea330ae73b6dff5d3970c82fa7319c883c
Luan <nguyenthanhluan230703@gmail.com>
2022-10-18 16:39:44 +0700

    Update

---
+++ PlatformGame/ObjectFactory.cpp
@@ -0,0 +1,27 @@
+﻿#include"stdafx.h"
+#include "ObjectFactory.h"
+ObjectFactory* ObjectFactory::sInstance = nullptr;
+
+Entity* ObjectFactory::CreateObject(std::string type, Properties* props)
+{
+	Entity* object = nullptr;
+
+	auto it = mTypeRegistry.find(type);//tìm type class 
+
+
+	if (it != mTypeRegistry.end())
+	{ //chạy đến cuối m_TypeRegistry
+		object = it->second(props);
+	}
+	return object;
+}
+
+void ObjectFactory::RegisterType(std::string className, std::function<Entity* (Properties* props)> type)
+{
+	mTypeRegistry[className] = type;
+}
+
+ObjectFactory* ObjectFactory::getInstance()
+{
+	return sInstance = (sInstance != nullptr) ? sInstance : new ObjectFactory();
+}
---
+++ PlatformGame/ObjectFactory.h
@@ -0,0 +1,31 @@
+﻿#ifndef OBJECTFACTORY_H
+#define OBJECTFACTORY_H
+#include "Entity.h"
+
+class ObjectFactory
+{
+private:
+	ObjectFactory() {};
+	static ObjectFactory* sInstance;
+	std::map < std::string, std::function<Entity* (Properties* props) >> mTypeRegistry;
+public:
+	Entity* CreateObject(std::string type, Properties* props);
+	void RegisterType(std::string className, std::function<Entity* (Properties* props)> type);//thông tin về hàm function(con trỏ hàm) std::function<tên hàm* (tham số hàm)> tên con trỏ hàm  https://howkteam.vn/course/khoa-hoc-lap-trinh-c-can-ban/con-tro-ham-trong-c-function-pointers-3916
+	static ObjectFactory* getInstance();
+
+
+};
+template <class Type>//cho phép bạn định nghĩa một hàm, một lớp mà không cần chỉ ra đối số kiểu dữ liệu là gì. Tuỳ vào kiểu dữ liệu mà người dùng truyền vào thì nó sẽ hoạt động theo kiểu dữ liệu đó.
+class Registrar
+{
+public:
+	Registrar(std::string className);
+};
+
+#endif
+
+template<class Type>
+inline Registrar<Type>::Registrar(std::string className)
+{
+	ObjectFactory::getInstance()->RegisterType(className, [](Properties* props)->Entity* { return new Type(props); });//lambda function [](Properties* props)->GameObject* { return new Type(props); } hàm ẩn danh không cần định nghĩa,->GameObject là kiểu trả về của hàm lambda https://en.cppreference.com/w/cpp/language/lambda
+}
